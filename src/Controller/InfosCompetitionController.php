bbb13f911fc8f709a27a0b01f17f7abb66bb8df7
Kenzo Vongkingkeo <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-03-21 11:32:31 +0100

    infos compet

--- src/Controller/InfosCompetitionController.php
+++ src/Controller/InfosCompetitionController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CompetitionRepository;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Competition;
 
 class InfosCompetitionController extends AbstractController
 {
     #[Route('/infos/competition', name: 'app_infos_competition')]
-    public function index(): Response
+    public function index(CompetitionRepository $repo): Response
     {
+        $competitions = $repo->findAllWithoutId();
+
         return $this->render('infoscompetition/infoscompetition.html.twig', [
-            'controller_name' => 'InfosCompetitionController',
+            'competitions' => $competitions,
         ]);
     }
 }
