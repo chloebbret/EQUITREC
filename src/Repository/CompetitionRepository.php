44f4a2bcb4efdb843ff01cd6bd93993971474e6a
Kenzo Vongkingkeo <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-03-21 11:25:43 +0100

    modif entite et repo competition

--- src/Repository/CompetitionRepository.php
+++ src/Repository/CompetitionRepository.php
@@ -38,6 +38,14 @@ class CompetitionRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
+        public function findAllWithoutId()
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->select('c.nom_competition, c.adr_competition, c.cp_competition, c.ville_competition, c.debut_competition, c.fin_competition, c.nb_epreuves');
+
+        return $qb->getQuery()->getResult();
+    }
+
 
 //    /**
 //     * @return Competition[] Returns an array of Competition objects
