45a01048aab0afb9ed90acd9792871117b252634
KurosakiKenzo69 <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-03-28 18:15:23 +0200

    debut Classement

--- src/Controller/ClassementController.php
+++ src/Controller/ClassementController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Competiteur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CompetiteurRepository;
+
 
 class ClassementController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/classement', name: 'app_classement')]
     public function index(): Response
     {
+        $repository = $this->entityManager->getRepository(Competiteur::class);
+        $competiteurs = $repository->findAll();
+
         return $this->render('classement/index.html.twig', [
             'controller_name' => 'ClassementController',
+            'competiteurs' => $competiteurs
         ]);
     }
+
 }
+
+
