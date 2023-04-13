86ce990bcfd942f7c70d0c4e5c175583c1e78c32
Chloe <b.bernard@lyon.ort.asso.fr>
2023-04-13 19:34:46 +0200

    commit accueil + menu(- quelques pages)

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
